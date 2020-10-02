2159eb74d3d62c8bdb8ad3f1151dbdca039a68e8
Graham Cox <graham@grahamcox.co.uk>
2020-10-02 08:20:55 +0100

    Model to represent a user, and use case to load one by ID

---
+++ service/internal/users/usecases.go
@@ -0,0 +1,17 @@
+package users
+
+import (
+	"context"
+	"errors"
+)
+
+var (
+	// ErrUserNotFound indicates that no user was found with the provided ID.
+	ErrUserNotFound = errors.New("users: not found")
+)
+
+// GetUserUseCase is the use case used to load a single user by ID.
+type GetUserUseCase interface {
+	// GetUserByID allows us to load the user with the given ID.
+	GetUserByID(ctx context.Context, id UserID) (UserModel, error)
+}
