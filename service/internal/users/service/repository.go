61bb29396abbfe30598e6de371fcdfae53f3c9c8
Graham Cox <graham@grahamcox.co.uk>
2020-10-07 08:04:57 +0100

    Added service calls for getting users

--- service/internal/users/service/repository.go
+++ service/internal/users/service/repository.go
@@ -10,4 +10,7 @@ import (
 type repository interface {
 	// Get the user that has the provided unique ID.
 	GetUserByID(ctx context.Context, id users.UserID) (users.UserModel, error)
+
+	// Get the user that has the provided authentication details.
+	FindUserByAuthentication(ctx context.Context, providerID, userID string) (users.UserModel, error)
 }
--- service/internal/users/service/service.go
+++ service/internal/users/service/service.go
@@ -1,6 +1,22 @@
 package service
 
+import (
+	"context"
+
+	"github.com/sazzer/nrl/service/internal/users"
+)
+
 // The actual implementation of the Users Service.
 type service struct {
 	repository repository
+}
+
+// GetUserByID allows us to load the user with the given ID.
+func (s service) GetUserByID(ctx context.Context, id users.UserID) (users.UserModel, error) {
+	return s.repository.GetUserByID(ctx, id)
+}
+
+// Get the user that has the provided authentication details.
+func (s service) FindUserByAuthentication(ctx context.Context, providerID, userID string) (users.UserModel, error) {
+	return s.repository.FindUserByAuthentication(ctx, providerID, userID)
 }
