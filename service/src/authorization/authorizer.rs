077581439c13c416f6992babf333f7f05f409d21
Graham Cox <graham@grahamcox.co.uk>
2020-10-19 20:24:56 +0100

    Disable clippy warning because it's wrong on stable rust

--- service/src/authorization/authorizer.rs
+++ service/src/authorization/authorizer.rs
@@ -51,6 +51,7 @@ impl<'a> Authorizing<'a> {
     }
 
     /// Check if this request has been authorized, but regardless of who it was authorized for.
+    #[allow(clippy::missing_const_for_fn)] // Clippy is wrong on stable rust
     pub fn authorized(self) -> Self {
         let result = self.0.is_some();
         self.perform_check(result)
