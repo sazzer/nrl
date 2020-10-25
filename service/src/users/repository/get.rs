321db3f1b9d19f466f35efb80d139f632ff0ba71
Graham Cox <graham@grahamcox.co.uk>
2020-10-25 10:07:12 +0000

    Able to authenticate as an existing user

--- service/src/users/repository/get.rs
+++ service/src/users/repository/get.rs
@@ -1,5 +1,6 @@
 use super::UsersRepository;
-use crate::users::{UserID, UserModel};
+use crate::users::{ProviderID, ProviderUserID, UserID, UserModel};
+use serde_json::json;
 
 impl UsersRepository {
     /// Get the user out of the database that has the given User ID.
@@ -25,6 +26,46 @@ impl UsersRepository {
             .map(UserModel::from);
 
         tracing::debug!(user_id = ?user_id, user = ?row, "Got user by ID");
+
+        row
+    }
+
+    /// Get the user out of the database that has the given authentication details.
+    ///
+    /// # Parameters
+    /// - `provider_id` - The ID of the authentication provider that the user is authenticating with
+    /// - `user_id` - The ID of the user with this authentication provider
+    ///
+    /// # Returns
+    /// The user, if it was found. `None` if it doesn't exist.
+    pub async fn get_user_by_authentication(
+        &self,
+        provider_id: &ProviderID,
+        user_id: &ProviderUserID,
+    ) -> Option<UserModel> {
+        tracing::debug!(provider_id = ?provider_id, user_id = ?user_id, "Getting user by authentication details");
+
+        let conn = self
+            .database
+            .checkout()
+            .await
+            .expect("Failed to get connection");
+
+        let authentication_bind = json!([{
+          "provider": provider_id,
+          "user": user_id
+        }]);
+
+        let row = conn
+            .query_opt(
+                "SELECT * FROM users WHERE authentications @> $1",
+                &[&authentication_bind],
+            )
+            .await
+            .unwrap()
+            .map(UserModel::from);
+
+        tracing::debug!(provider_id = ?provider_id, user_id = ?user_id, user = ?row, "Got user by authentication details");
 
         row
     }
--- service/src/users/service/authenticate.rs
+++ service/src/users/service/authenticate.rs
@@ -19,10 +19,72 @@ impl AuthenticateUserUseCase for UsersService {
     /// The user that we have authenticated as.
     async fn authenticate_user(
         &self,
-        _authentication: Authentication,
+        authentication: Authentication,
         _display_name: DisplayName,
         _email: Option<Email>,
     ) -> Result<UserModel, AuthenticateUserError> {
-        todo!()
+        if let Some(user) = self
+            .repository
+            .get_user_by_authentication(&authentication.provider, &authentication.user)
+            .await
+        {
+            // A user with these authentication details already exists, so return it as-is
+            Ok(user)
+        } else {
+            // A user with these authentication details don't exist, so lets create a new one.
+            todo!()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::infrastructure::database::testing::TestDatabase;
+    use crate::users::{config::Config, Authentication};
+    use assert2::{check, let_assert};
+    use nrl_testdatabase::seeddata::SeedUser;
+
+    #[actix_rt::test]
+    async fn test_authenticate_known_user() {
+        let seed_user =
+            SeedUser::default().with_authentication("twitter", "twitterUserId", "Twitter User");
+
+        let db = TestDatabase::new().await;
+        db.seed(&seed_user).await;
+
+        let sut = Config::new(db.db).service;
+
+        let user_id = seed_user.user_id.clone().into();
+        let result = sut
+            .authenticate_user(
+                Authentication {
+                    provider: "twitter".parse().unwrap(),
+                    user: "twitterUserId".parse().unwrap(),
+                    display_name: "Twitter User".parse().unwrap(),
+                },
+                "Twitter User".parse().unwrap(),
+                None,
+            )
+            .await;
+
+        let_assert!(Ok(user) = result);
+
+        check!(user.identity.id == user_id);
+        check!(user.identity.version == seed_user.version);
+        check!(user.identity.created == seed_user.created);
+        check!(user.identity.updated == seed_user.updated);
+
+        check!(user.data.username == seed_user.username.map(|v| v.parse().unwrap()));
+        check!(user.data.email == seed_user.email.map(|v| v.parse().unwrap()));
+        check!(user.data.display_name == seed_user.display_name.parse().unwrap());
+        check!(
+            user.data.authentications
+                == vec![Authentication {
+                    provider: "twitter".parse().unwrap(),
+                    user: "twitterUserId".parse().unwrap(),
+                    display_name: "Twitter User".to_owned(),
+                },]
+        );
     }
 }
