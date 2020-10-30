7b2cc02c29a64e2deaeee0de6071b84e0e1e0347
Graham Cox <graham@grahamcox.co.uk>
2020-10-30 08:04:21 +0000

    Set up i18n

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 export const App: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">{t("hello")}</header>
     </div>
   );
 };
