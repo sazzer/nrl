94515a20af1c569812220d2e9159662365271f79
Graham Cox <graham@grahamcox.co.uk>
2020-10-30 17:09:21 +0000

    Structure for the landing page

--- webapp/src/ui/App.tsx
+++ webapp/src/ui/App.tsx
@@ -1,10 +1,12 @@
 import { HeaderBar } from "./header";
+import { LandingPage } from "./landing";
 import React from "react";
 
 export const App: React.FC = () => {
   return (
     <>
       <HeaderBar />
+      <LandingPage />
     </>
   );
 };
---
+++ webapp/src/ui/landing/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+/**
+ * The landing page, containing the available login options.
+ */
+export const LandingPage: React.FC = () => {
+  /* eslint-disable jsx-a11y/alt-text */
+  return (
+    <div className="row">
+      <div className="col-12 col-lg-3 order-lg-3">Authentication here</div>
+      <div className="col-12 col-lg-9">
+        <img
+          src="https://i.redd.it/7720hbhrl8v51.jpg"
+          role="presentation"
+          className="img-fluid img-thumbnail rounded shadow"
+        />
+      </div>
+    </div>
+  );
+};
